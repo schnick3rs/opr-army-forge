0e7a9b92204d5b3754f303174c82efeab64fbe61
Adam Lay <adam93.lay+git@gmail.com>
2022-03-21 20:37:18 +0000

    Re-add PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-if (false) {
-  const withPWA = require("next-pwa");
-  const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-  module.exports = withPWA({
-    pwa: {
-      dest: "public",
-      runtimeCaching,
-    },
-  });
-}
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+});
