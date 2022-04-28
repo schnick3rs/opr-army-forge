f2159b4bb9ca44f241eb8120325e3c58c561b39c
Adam Lay <adam93.lay+git@gmail.com>
2022-04-28 21:25:47 +0100

    Update ReleaseNotes.tsx

--- views/components/ReleaseNotes.tsx
+++ views/components/ReleaseNotes.tsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../data/store";
 import { setOpenReleaseNotes } from "../../data/appSlice";
 
-export const APP_VERSION = "0.6.0";
+export const APP_VERSION = "0.6.1";
 
 export default function ReleaseNotes() {
   const open = useSelector((state: RootState) => state.app.openReleaseNotes);
@@ -66,6 +66,13 @@ interface IRelease {
 }
 
 const releaseNotes: IRelease[] = [
+  {
+    version: "0.6.1",
+    date: "28/04/22",
+    notes: [
+      "Fixed rule text display on hover on desktop.",
+    ],
+  },
   {
     version: "0.6.0",
     date: "28/04/22",
